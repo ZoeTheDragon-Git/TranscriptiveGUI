d55fd6a3af7166b145aa77d6d21886d2451ca82f
ZoeTheDragon-Git <github@dragonslair.cc>
2026-03-01 18:01:01 -0500

    Added side panel and implemented text+colors

--- obj/Debug/net10.0/LinuxGUI.AssemblyInfo.cs
+++ obj/Debug/net10.0/LinuxGUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinuxGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c601a4e975868ca2a6e7b2257abea8146872bc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinuxGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinuxGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
